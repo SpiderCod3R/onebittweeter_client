686a0661b66403914373b21a914bdfd09027b651
Thunder Master <thndrmaster@gmail.com>
2019-10-29 15:55:48 -0300

    Odisseia - Concetando a API com .env

--- src/containers/TrendingTopicsContainer/index.jsx
+++ src/containers/TrendingTopicsContainer/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import TrendingTopics from '../../components/TrendingTopics'
-
+import axios from 'axios';
 
 class TrendingTopicsContainer extends Component {
   constructor(props) {
@@ -12,9 +12,18 @@ class TrendingTopicsContainer extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      hashtags: [['#OneBitCode', '10k'], ['#RubyOnRails', '10k']]
-    })
+    axios.get(process.env.REACT_APP_API + '/trending')
+      .then((response) => {
+        console.log(response.data.hashtags);
+        this.setState(() => {
+          return {
+            hashtags: response.data.hashtags
+          }
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   render() {
