deabffd553c266fc8aa37a059cff366d99314d11
Thunder Master <thndrmaster@gmail.com>
2019-10-30 10:56:33 -0300

    Odisseia - Integrando o DevTools

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,13 +4,9 @@ import { routerMiddleware } from 'connected-react-router'
 import createRootReducer from './reducers';
 import thunk from 'redux-thunk';
 
-// Routes imports
-// import { routerMiddleware } from 'react-router-redux'
-
 export const history = createBrowserHistory()
 const history_middleware = routerMiddleware(history)
-
-// const Store = createStore(createRootReducer, applyMiddleware(history_middleware, thunk));
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose;
 
 export default function configureStore(preloadedState) {
   const store = createStore(
@@ -21,12 +17,8 @@ export default function configureStore(preloadedState) {
         history_middleware, 
         thunk
       ),
+      devtools,
     ),
   )
-
   return store
 }
-
-
-
-// export default Store;
